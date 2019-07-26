f3313897edff87edda0e8593a650671a90d052a4
hoang thien <ducthien.hoang@ved.com.vn>
2019-07-26 20:10:18 +0700

    add firestore sample

--- app/src/main/java/vn/ochabot/seaconnect/challenges/ChallengesActivity.java
+++ app/src/main/java/vn/ochabot/seaconnect/challenges/ChallengesActivity.java
@@ -1,17 +1,20 @@
 package vn.ochabot.seaconnect.challenges;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.*;
 import org.jetbrains.annotations.Nullable;
 import vn.ochabot.seaconnect.R;
 import vn.ochabot.seaconnect.core.base.BaseActivity;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Thien.
@@ -22,6 +25,8 @@ public class ChallengesActivity extends BaseActivity {
 
     FirebaseFirestore db;
     CollectionReference matches;
+    Map<String, QueryDocumentSnapshot> mData = new HashMap<>();
+    MatchAdapter adapter = new MatchAdapter();
 
 
     @Override
@@ -41,9 +46,20 @@ public class ChallengesActivity extends BaseActivity {
     private void addMatch() {
         Map<String, Object> data = new HashMap<>();
         data.put("host", "thien");
-        data.put("title","Bi lac - tran dau dinh cao");
-        data.put("timestamp",System.currentTimeMillis());
-        matches.document(String.valueOf(System.nanoTime())).set(data);
+        data.put("title", "Bi lac - tran dau dinh cao");
+        data.put("timestamp", System.currentTimeMillis());
+        matches.document(String.valueOf(System.nanoTime())).set(data).addOnFailureListener(new OnFailureListener() {
+
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "add failed.", e);
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.e(TAG, "add success");
+            }
+        });
     }
 
     private void showMatches() {
@@ -54,12 +70,17 @@ public class ChallengesActivity extends BaseActivity {
                     Log.e(TAG, "Listen failed.", e);
                     return;
                 }
-                List<String> cities = new ArrayList<>();
                 for (QueryDocumentSnapshot doc : values) {
-                    if (doc.get("name") != null) {
-                        cities.add(doc.getString("host"));
-                    }
+                    mData.put(doc.getId(), doc);
                 }
+                adapter.mData = new ArrayList<>(mData.values());
+                 Collections.sort(adapter.mData, new Comparator<QueryDocumentSnapshot>() {
+                    @Override
+                    public int compare(QueryDocumentSnapshot d1, QueryDocumentSnapshot d2) {
+                        return (int) (Long.valueOf(d1.get("timestamp").toString()) - Long.valueOf(d2.get("timestamp").toString()));
+                    }
+                });
+                adapter.notifyDataSetChanged();
             }
         });
     }
@@ -75,21 +96,33 @@ public class ChallengesActivity extends BaseActivity {
     }
 
 
+    public static class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.MyViewHolder> {
 
-    public static class User {
-        public String userName;
-        public int team;
-    }
+        List<QueryDocumentSnapshot> mData;
 
-    public static class Match {
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return null;
+        }
 
-        public String host;
-        public List<User> users;
-        public String reward;
-        public long timeStamp;
-        public String title;
-        public int winner;
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
 
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder queryDocumentSnapshotMyViewHolder, int i) {
+
+        }
+
+
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
     }
 
 
